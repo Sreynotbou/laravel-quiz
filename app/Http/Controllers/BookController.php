b36af351011abdb5e3fb291fd9d34ea87e4ff928
Sreynot <sreynot.bou@student.passerellesnumeriques.org>
2021-11-05 11:44:51 +0700

    QUiz-laravel

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Book::get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = new Book();
+        $book->author_id= $request->author_id;
+        $book->title = $request->title;
+        $book->body = $request->body;
+        $book->save();
+        
+        return response()->json("Book Created");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Book::findOrFail($id);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $author = Book::findOrFail($id);;
+        $book->author_id= $request->author_id;
+        $book->title = $request->title;
+        $book->body = $request->body;
+        $book->save();
+        
+        return response()->json("Book Created");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Book::destroy($id);
+        
+    }
+}
